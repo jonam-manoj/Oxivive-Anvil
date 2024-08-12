bbb5372edfcdce7148613f9ed709b5d6952ef100
manoj4ev@gmail.com <manoj4ev@gmail.com>
2024-08-12 07:00:47 +0000

    Edited settings and profile

--- client_code/profile/__init__.py
+++ client_code/profile/__init__.py
@@ -12,3 +12,19 @@ class profile(profileTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def link_2_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('notifications')
+
+  def link_3_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('bookings')
+
+  def link_4_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('reports')
+
+  def link_6_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('home')
